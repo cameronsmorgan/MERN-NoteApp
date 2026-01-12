e478ba6251e2cb9560f21b6846c749dca100272f
cameronsmorgan <cameronsmorgan514@gmail.com>
2026-01-12 20:50:20 +0200

    API routes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,19 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res)=>{
-    res.send("You have 5 notes");
+    res.status(200).send("You have 30 notes");
+})
+
+app.post("/api/notes", (req, res) =>{
+    res.status(201).json({message:"Note created successfully!"})
+})
+
+app.put("/api/notes/:id", (req, res)=>{
+    res.status(200).json({message:"Note updated successfully!"})
+})
+
+app.delete("/api/notes/:id",(req, res) =>{
+    res.status(200).json({message:"Note deleted successfuly!"})
 })
 
 app.listen(5001, () =>{
