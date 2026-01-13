51ff1a27cbb318b3e69bdb888092865822df1e85
cameronsmorgan <cameronsmorgan514@gmail.com>
2026-01-13 17:21:55 +0200

    Created models from Schema and set up MONGO

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,26 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+//console.log(process.env.MONGO_URI);
 
 //const express = require("express");    --> means the same as above just the commonJS syntax that is changed to module in the package.json
 
 const app = express();
+const port = process.env.PORT || 5001;
+//console.log(port);
+
+connectDB();
 
 app.use("/api/notes", notesRoutes)
 
 
 
-app.listen(5001, () =>{
-    console.log("Server started on PORT:5001");
+app.listen(port, () =>{
+    console.log("Server started on PORT:", port);
 })
+
+
