3ee2b3e16290c94844c21d1cce4ea80f160fa382
cameronsmorgan <cameronsmorgan514@gmail.com>
2026-01-23 14:28:25 +0200

    navbar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const HomePage = () => {
   return (
-    <div>
-      <p>Homepage</p>
+    <div className='min-h-screen'>
+      <Navbar />
     </div>
   )
 }
