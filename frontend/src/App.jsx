4a82f5e609a78a898fb42ef0e8fb6e929abffd84
cameronsmorgan <cameronsmorgan514@gmail.com>
2026-01-28 21:18:04 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import NoteDetailPage from './pages/NoteDetailPage'
 
 const App = () => {
   return (
-    <div data-theme="coffee">
+    <div data-theme="forest">
 
       <Routes>
         <Route path='/' element={<HomePage />}/>
